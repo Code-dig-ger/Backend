59cd71b621183e5771f7b513fe7fbba268801d6e
unknown <shivamsinghal1012@gmail.com>
2020-12-19 11:59:52 +0530

    minor change

--- codedigger/codedigger/settings.py
+++ codedigger/codedigger/settings.py
@@ -159,6 +159,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 REST_FRAMEWORK = {
--- codedigger/problem/views.py
+++ codedigger/problem/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics,mixins,permissions 
