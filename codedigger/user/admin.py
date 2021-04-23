2744f4680d18ae37a8192e38eb0afb84d9871f60
Aaradhya Beri <aaradhyaberi@gmail.com>
2021-04-23 14:09:29 +0530

    more fixes

--- codedigger/user/admin.py
+++ codedigger/user/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import User,Profile,UserFriends
 
 class UserAdmin(admin.ModelAdmin):
-    search_fields = ('username',)
+    search_fields = ('username','email',)
 
 class ProfileAdmin(admin.ModelAdmin):
     search_fields = ('owner__username','name',)
