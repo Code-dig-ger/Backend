8edd5793217ae5a8d7748fe6ecaae4f577e20375
shivamsinghal1 <shivamsinghal1012@gmail.com>
2021-11-16 15:23:27 +0530

    added test case for codeforcesProblemSet

--- codedigger/codeforces/codeforcesProblemSet.py
+++ codedigger/codeforces/codeforcesProblemSet.py
@@ -1,21 +1,11 @@
 from problem.models import Problem
 from .models import CodeforcesProblemSet
 
-def get_child_parent(problem):
+def get_parent(problem):
     try : 
         return CodeforcesProblemSet.objects.get(child = problem).parent
     except : 
         return problem
-
-
-def get_parent(problem):
-    # return parent of this problem 
-    # else return self 
-    parent = get_child_parent(problem)
-    while parent != problem:
-        problem = parent
-        parent = get_child_parent(problem)
-    return parent
 
 
 def check(problem1, problem2):
@@ -26,6 +16,7 @@ def join(problem1, problem2):
     if not check(problem1, problem2):
         parent = get_parent(problem1)
         child = get_parent(problem2)
+        CodeforcesProblemSet.objects.filter(parent = child).update(parent = parent)
         CodeforcesProblemSet(parent = parent, child = child).save()
 
 
--- codedigger/codeforces/tests/test_scraper_utils.py
+++ codedigger/codeforces/tests/test_scraper_utils.py
@@ -1,5 +1,8 @@
 from .test_setup import TestSetUp
+
+from problem.models import Problem
 from codeforces.scraper_utils import isSameProblem
+from codeforces.codeforcesProblemSet import join, get_similar_problems
 
 
 class TestViews(TestSetUp):
@@ -14,3 +17,15 @@ class TestViews(TestSetUp):
 
         url1 = "https://codeforces.com/contest/1603/problem/B"
         self.assertFalse(isSameProblem(url1, url2))
+    
+    def test_join_problem(self):
+        probA = Problem.objects.get(prob_id = '100001A')
+        probC = Problem.objects.get(prob_id = '1549C')
+        probD = Problem.objects.get(prob_id = '1549D')
+        probE = Problem.objects.get(prob_id = '1549E')
+
+        join(probA, probC)
+        join(probD, probE)
+        join(probA, probE)
+
+        self.assertEqual(get_similar_problems(probC).count(), 3)
