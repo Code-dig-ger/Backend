d643a82b503497741ea5c8c18dfc93d894c39622
Aman Jolly <aman.amanjolly@gmail.com>
2021-12-13 17:49:43 +0530

    refactor

--- codedigger/atcoder/scrapers.py
+++ codedigger/atcoder/scrapers.py
@@ -1,8 +1,5 @@
 import requests
-from math import log2
-from problem.models import Problem, atcoder_contest
-from .api import *
-from utils.common import rating_to_difficulty
+from user.exception import ValidationException
 
 
 def get_user_history(handle):
