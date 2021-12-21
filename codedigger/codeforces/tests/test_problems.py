39aaba03ac2a641cb1c34bc3ef49a0944bc02567
Suryansh Omar <76695520+Suryanshomar7240@users.noreply.github.com>
2021-12-21 13:15:15 +0530

    Update test_problems.py

--- codedigger/codeforces/tests/test_problems.py
+++ codedigger/codeforces/tests/test_problems.py
@@ -5,3 +5,4 @@ from codeforces.test_fixtures.models_utils_fixture import cf_user
 class Test_assign_problem(TestSetUp):
     def test_Assign(self):
         self.assertEqual((CodeforcesAssignProblem(cf_user)),[])
+        
