fde0072d5ca11f6b1a1c55677516af0da7e49149
Anubhav Tyagi <tyagianubhav80@gmail.com>
2022-01-09 00:23:47 +0530

    added name to upsolve route

--- codedigger/codechef/urls.py
+++ codedigger/codechef/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('upsolve', views.CodechefUpsolveAPIView.as_view()),
+    path('upsolve', views.CodechefUpsolveAPIView.as_view(), name="codechef-upsolve"),
     path('testing', views.testing)
 ]
